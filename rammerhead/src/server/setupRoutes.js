a91891e037b279a4a36ca324b6d88ea01ba1b1b5
LucasR1090133 <1090133@lwsd.org>
2026-03-20 22:54:35 +0000

    SINE1

--- rammerhead/src/server/setupRoutes.js
+++ rammerhead/src/server/setupRoutes.js
@@ -1,12 +1,92 @@
-// Existing code...
+const generateId = require('../util/generateId');
+const URLPath = require('../util/URLPath');
+const httpResponse = require('../util/httpResponse');
+const config = require('../config');
+const StrShuffler = require('../util/StrShuffler');
+const RammerheadSession = require('../classes/RammerheadSession');
 
-proxyServer.GET('/mainport', (req, res) => {
-    // existing code
-});
+module.exports = function setupRoutes(proxyServer, sessionStore, logger) {
+    const isNotAuthorized = (req, res) => {
+        if (!config.password) return;
+        const { pwd } = new URLPath(req.url).getParams();
+        if (config.password !== pwd) {
+            httpResponse.accessForbidden(logger, req, res, config.getIP(req), 'bad password');
+            return true;
+        }
+        return false;
+    };
 
-proxyServer.GET('/shuffleDict', (req, res) => {
-    const shuffleDict = StrShuffler.generateDictionary();
-    res.end(JSON.stringify(shuffleDict));
-});
+    if (process.env.DEVELOPMENT) {
+        proxyServer.GET('/garbageCollect', (req, res) => {
+            global.gc();
+            res.end('Ok');
+        });
+    }
 
-// Existing code...
+    proxyServer.GET('/needpassword', (req, res) => {
+        res.end(config.password ? 'true' : 'false');
+    });
+
+    proxyServer.GET('/newsession', (req, res) => {
+        if (isNotAuthorized(req, res)) return;
+        const id = generateId();
+        const session = new RammerheadSession();
+        session.data.restrictIP = config.getIP(req);
+        sessionStore.addSerializedSession(id, session.serializeSession());
+        res.end(id);
+    });
+
+    proxyServer.GET('/editsession', (req, res) => {
+        if (isNotAuthorized(req, res)) return;
+        let { id, httpProxy, enableShuffling } = new URLPath(req.url).getParams();
+        if (!id || !sessionStore.has(id)) {
+            return httpResponse.badRequest(logger, req, res, config.getIP(req), 'not found');
+        }
+        const session = sessionStore.get(id);
+        if (httpProxy) {
+            if (httpProxy.startsWith('http://')) {
+                httpProxy = httpProxy.slice(7);
+            }
+            session.setExternalProxySettings(httpProxy);
+        } else {
+            session.externalProxySettings = null;
+        }
+        if (enableShuffling === '1' && !session.shuffleDict) {
+            session.shuffleDict = StrShuffler.generateDictionary();
+        }
+        if (enableShuffling === '0') {
+            session.shuffleDict = null;
+        }
+        res.end('Success');
+    });
+
+    proxyServer.GET('/deletesession', (req, res) => {
+        if (isNotAuthorized(req, res)) return;
+        const { id } = new URLPath(req.url).getParams();
+        if (!id || !sessionStore.has(id)) {
+            res.end('not found');
+            return;
+        }
+        sessionStore.delete(id);
+        res.end('Success');
+    });
+
+    proxyServer.GET('/sessionexists', (req, res) => {
+        const id = new URLPath(req.url).get('id');
+        if (!id) {
+            httpResponse.badRequest(logger, req, res, config.getIP(req), 'Must specify id parameter');
+        } else {
+            res.end(sessionStore.has(id) ? 'exists' : 'not found');
+        }
+    });
+
+    proxyServer.GET('/mainport', (req, res) => {
+        const serverInfo = config.getServerInfo(req);
+        res.end((serverInfo.port || '').toString());
+    });
+
+    proxyServer.GET('/shuffleDict', (req, res) => {
+        const shuffleDict = StrShuffler.generateDictionary();
+        res.end(JSON.stringify(shuffleDict));
+    });
+};
